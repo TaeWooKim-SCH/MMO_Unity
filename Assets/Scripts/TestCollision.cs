55de9b05dfd50e8c3fe85156c804ee74c2289bb9
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-14 17:44:37 +0900

    add: Collision

---
+++ Assets/Scripts/TestCollision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollision : MonoBehaviour
+{
+    // 1) 나한테 RigidBody 있어야 한다 (IsKinematic: Off)
+    // 2) 나한테 Collider가 있어야 한다 (IsTrigger: Off)
+    // 3) 상대한테 Collider가 있어야 한다 (IsTrigger: Off)
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collision !");
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Trigger !");
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
