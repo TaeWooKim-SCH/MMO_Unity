13966490855d27dce88fc4504a19ac8694b21b6b
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-15 18:37:17 +0900

    add: Trigger

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestCollision : MonoBehaviour
-{
+public class TestCollision: MonoBehaviour {
     // 1) 나 혹은 상대한테 RigidBody 있어야 한다 (IsKinematic: Off)
     // 2) 나한테 Collider가 있어야 한다 (IsTrigger: Off)
     // 3) 상대한테 Collider가 있어야 한다 (IsTrigger: Off)
@@ -11,16 +10,17 @@ public class TestCollision : MonoBehaviour
         Debug.Log($"Collision {collision.gameObject.name} !");
     }
 
+    // 1) 둘 다 Collider가 있어야 한다
+    // 2) 둘 중 하나는 IsTrigger: On
+    // 3) 둘 중 하나는 RigidBody가 있어야 한다
     private void OnTriggerEnter(Collider other) {
         Debug.Log($"Trigger {other.gameObject.name} !");
     }
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
