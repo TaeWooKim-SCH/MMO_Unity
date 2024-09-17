720b97231803944b6708209546c36d1f09ed08d4
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-17 14:49:12 +0900

    add: Camera #2

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -6,13 +6,28 @@ public class PlayerController: MonoBehaviour {
     [SerializeField]
     float _speed = 10.0f;
 
+    bool _moveToDest = false;
+    Vector3 _destPos;
+
     void Start() {
         Managers.Input.KeyAction -= OnKeyboard;
         Managers.Input.KeyAction += OnKeyboard;
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
     void Update() {
-
+        if (_moveToDest) {
+            Vector3 dir = _destPos - transform.position;
+            if (dir.magnitude < 0.0001f) {
+                _moveToDest = false;
+            } else {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+                
+                transform.position += dir.normalized * moveDist;
+                transform.LookAt(_destPos);
+            }
+        }
     }
 
     void OnKeyboard() {
@@ -31,6 +46,28 @@ public class PlayerController: MonoBehaviour {
         if (Input.GetKey(KeyCode.D)) {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
             transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+
+        _moveToDest = false;
+    }
+
+    void OnMouseClicked(Define.MouseEvent evt) {
+        if (evt != Define.MouseEvent.Click) {
+            return;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100, Color.red, 1.0f);
+
+        LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+        //int mask = (1 << 8) | (1 << 9);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, mask)) {
+            _destPos = hit.point;
+            _moveToDest = true;
+            //Debug.Log($"Raycast Camera @ {hit.collider.gameObject.tag}");
         }
     }
 }
