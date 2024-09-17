2a60bde5c1fa2b183e1d40391d850773fdb420e4
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-17 13:03:42 +0900

    add: LayerMask

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -30,9 +30,12 @@ public class TestCollision: MonoBehaviour {
 
             Debug.DrawRay(Camera.main.transform.position, ray.direction * 100, Color.red, 1.0f);
 
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+            //int mask = (1 << 8) | (1 << 9);
+
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 100.0f)) {
-                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            if (Physics.Raycast(ray, out hit, 100.0f, mask)) {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.tag}");
             }
         }
         //if (Input.GetMouseButtonDown(0)) {
