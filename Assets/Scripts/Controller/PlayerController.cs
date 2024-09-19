8713e4cb4338efc36ad1a2411d342081f5d2602e
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-19 17:29:56 +0900

    add: State Machine #2

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -14,15 +14,13 @@ public class PlayerController: MonoBehaviour {
         Managers.Input.MouseAction += OnMouseClicked;
     }
 
-    float wait_run_ratio = 0;
-
     public enum PlayerState {
         Die,
         Moving,
         Idel,
     }
 
-    PlayerState _state;
+    PlayerState _state = PlayerState.Idel;
 
     void UpdateDie() {
         // 아무것도 못함
@@ -39,18 +37,15 @@ public class PlayerController: MonoBehaviour {
         }
 
         // 애니메이션
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
         Animator anim = GetComponent<Animator>();
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
+        // 현재 게임 상태에 대한 정보를 넘겨준다
+        anim.SetFloat("speed", _speed);
     }
 
     void UpdateIdel() {
         // 애니메이션
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
         Animator anim = GetComponent<Animator>();
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
+        anim.SetFloat("speed", 0);
     }
 
     void Update() {
