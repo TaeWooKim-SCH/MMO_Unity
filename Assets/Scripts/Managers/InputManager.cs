1d0de04eba2d140b6ae641cd15aff40746d992f0
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-21 00:28:09 +0900

    add: UI/Button Event

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager: MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class InputManager: MonoBehaviour
 
     public void OnUpdate()
     {
+        if (EventSystem.current.IsPointerOverGameObject()) {
+            return;
+        }
         //if (Input.anyKey && KeyAction != null) {
         //    KeyAction.Invoke();
         //}
