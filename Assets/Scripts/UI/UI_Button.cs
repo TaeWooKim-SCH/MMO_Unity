56b347900fc26d1845b2e434ec80f9f98fd73f2d
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-21 16:28:36 +0900

    add: UI 자동화 #3

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -2,29 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UI_Button : UI_Base
 {
     enum Buttons {
-        PointButton
+        PointButton,
     }
 
     enum Texts {
         PointText,
-        ScoreText
+        ScoreText,
     }
 
     enum GameObjects {
-        TestObject
+        TestObject,
+    }
+
+    enum Images {
+        ItemIcon,
     }
 
     private void Start() {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
 
         GetText((int)Texts.ScoreText).text = "Bind Test";
+
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+        UI_EventHandler evt = go.GetComponent<UI_EventHandler>();
+        evt.OnDragHandler += ((PointerEventData data) => { evt.gameObject.transform.position = data.position; });
     }
 
     int _score = 0;
